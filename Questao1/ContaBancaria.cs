7eb6ce08c2f3592beab11e2a013cdd5ebfff6029
suhettgabriel <suhettgabriel1983@gmail.com>
2024-10-28 07:49:04 -0300

    Commit 1

---
+++ Questao1/ContaBancaria.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+
+namespace Questao1
+{
+    class ContaBancaria {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        private const double TAXA_SAQUE = 3.50;
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0.0;
+        }
+
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double quantia)
+        {
+            if (quantia > 0)
+            {
+                Saldo += quantia;
+            }
+            else
+            {
+                Console.WriteLine("O valor do depósito deve ser positivo.");
+            }
+        }
+
+        public void Saque(double quantia)
+        {
+            if (quantia > 0)
+            {
+                Saldo -= quantia + TAXA_SAQUE;
+            }
+            else
+            {
+                Console.WriteLine("O valor do saque deve ser positivo.");
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Numero
+                + ", Titular: " + Titular
+                + ", Saldo: $" + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Questao2/Program.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json.Linq;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        string teamName = "Paris Saint-Germain";
+        int year = 2013;
+        int totalGoals = await GetTotalScoredGoals(teamName, year);
+
+        Console.WriteLine("Team " + teamName + " scored " + totalGoals + " goals in " + year);
+
+        teamName = "Chelsea";
+        year = 2014;
+        totalGoals = await GetTotalScoredGoals(teamName, year);
+
+        Console.WriteLine("Team " + teamName + " scored " + totalGoals + " goals in " + year);
+    }
+
+    public static async Task<int> GetTotalScoredGoals(string team, int year)
+    {
+        int totalGoals = 0;
+
+        totalGoals += await GetGoalsFromApi(team, year, "team1");
+
+        totalGoals += await GetGoalsFromApi(team, year, "team2");
+
+        return totalGoals;
+    }
+
+    private static async Task<int> GetGoalsFromApi(string team, int year, string teamPosition)
+    {
+        int goals = 0;
+        int page = 1;
+        bool hasMorePages = true;
+        HttpClient client = new HttpClient();
+
+        while (hasMorePages)
+        {
+            string url = $"https://jsonmock.hackerrank.com/api/football_matches?year={year}&{teamPosition}={team}&page={page}";
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode) return goals;
+
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+            JObject? jsonObject = JObject.Parse(jsonResponse);
+
+            if (jsonObject == null) return goals;
+
+            int totalPages = (int)(jsonObject["total_pages"] ?? 0);
+            foreach (var match in jsonObject["data"] ?? new JArray())
+            {
+                string? goalString = match[$"{teamPosition}goals"]?.ToString();
+
+                if (int.TryParse(goalString, out int matchGoals))
+                {
+                    goals += matchGoals;
+                }
+            }
+
+            page++;
+            hasMorePages = page <= totalPages;
+        }
+
+        return goals;
+    }
+}
